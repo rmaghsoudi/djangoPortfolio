1c5a5f1c83e10a608568797e78de678e10187bc5
Romy Maghsoudi <romy_mag@outlook.com>
2020-04-22 16:50:10 -0400

    added project name

--- projects/models.py
+++ projects/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Project(models.Model):
   # Images property which is of type imagefield
   image = models.ImageField(upload_to='images/')
+  name = models.CharField(max_length=20)
   # Summary property of type charfield
   summary = models.CharField(max_length=200)
 
   def __str__(self):
-    return self.summary
+    return self.name
