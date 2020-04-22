94dfdc742b42fbaa616a43aebb94b72ced0d07b6
Romy Maghsoudi <romy_mag@outlook.com>
2020-04-22 14:01:33 -0400

    connected to db and migrated data

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+  # Images property which is of type imagefield
+  image = models.ImageField(upload_to='images/')
+  # Summary property of type charfield
+  summary = models.CharField(max_length=200)
