333b7c85ac9d50b2c49e828e414169a076d411d3
Romy Maghsoudi <romy_mag@outlook.com>
2020-04-22 17:05:33 -0400

    added link to model and increased summary length

--- projects/models.py
+++ projects/models.py
@@ -5,8 +5,9 @@ class Project(models.Model):
   # Images property which is of type imagefield
   image = models.ImageField(upload_to='images/')
   name = models.CharField(max_length=20)
+  link = models.CharField(max_length=100)
   # Summary property of type charfield
-  summary = models.CharField(max_length=200)
+  summary = models.CharField(max_length=300)
 
   def __str__(self):
     return self.name
