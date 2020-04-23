81b5929e9c4cb475fe4642864608859dd0d11e62
Romy Maghsoudi <romy_mag@outlook.com>
2020-04-23 11:26:40 -0400

    heroku deployment setup

--- portfolioDjango/settings.py
+++ portfolioDjango/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
-
+import os, django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,3 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
